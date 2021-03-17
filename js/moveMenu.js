2226dbf7088c5179e63a68a90660fe571fc8a389
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-03-16 18:51:37 -0700

    Add terms of use

--- js/moveMenu.js
+++ js/moveMenu.js
@@ -8,3 +8,13 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleMobileNav() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
